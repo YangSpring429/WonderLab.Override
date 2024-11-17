cdd06087e8279ab087a4579434bf564ef944954e
YangSpring <99802662+YangSpring114@users.noreply.github.com>
2024-11-17 13:58:38 +0800

    添加实验性内容

---
+++ WonderLab/Controls/Media/Behaviors/WindowTitleBarBehavior.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WonderLab.Controls.Media.Behaviors;
+internal class WindowTitleBarBehavior {
+}
--- WonderLab/Views/Window/MainWindow.axaml.cs
+++ WonderLab/Views/Window/MainWindow.axaml.cs
@@ -1,13 +1,9 @@
 using Avalonia.Controls;
-using Avalonia.Platform;
-using System.Diagnostics;
 
 namespace WonderLab;
 
 public sealed partial class MainWindow : Window {
     public MainWindow() {
         InitializeComponent();
-        var r = TryGetPlatformHandle().Handle;
-        Debug.WriteLine(r);
     }
 }
