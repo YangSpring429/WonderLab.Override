5ab1b1dd66a54c795b35ba5e8ebe0c8bf246ee4d
YangSpring <99802662+YangSpring114@users.noreply.github.com>
2024-11-18 22:03:20 +0800

    添加 Linux X11 环境下的窗口处理器

--- WonderLab/Controls/Media/Behaviors/WindowTitleBarBehavior.cs
+++ WonderLab/Controls/Media/Behaviors/WindowTitleBarBehavior.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks;
-using Avalonia;
+﻿using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Avalonia.Markup.Xaml;
-
 using Avalonia.Xaml.Interactivity;
+using System;
+
+#if OSX
+using WonderLab.Platform.MacOS;
+#elif LINUX
+using WonderLab.Platform.Linux;
+#endif
 
 namespace WonderLab.Controls.Media.Behaviors;
 
@@ -15,37 +15,47 @@ public sealed class WindowTitleBarBehavior : Behavior<Window> {
     protected override void OnAttached() {
         base.OnAttached();
 
-        AssociatedObject.Opened+= OnOpened;
+        AssociatedObject.Opened += OnOpened;
         AssociatedObject.PropertyChanged += OnPropertyChanged;
     }
 
+    private bool IsWayland() {
+        string sessionType = Environment.GetEnvironmentVariable("XDG_SESSION_TYPE").ToLower();
+        return sessionType is "wayland";
+    }
+
     private void OnPropertyChanged(object sender, AvaloniaPropertyChangedEventArgs e) {
+#if OSX
         if (e.Property == Window.WindowStateProperty || e.Property == Window.BoundsProperty) {
             var platform = AssociatedObject.TryGetPlatformHandle();
             if (platform is not null) {
                 var nsWindow = platform.Handle;
                 if (nsWindow != IntPtr.Zero) {
                     try {
-                        AdjustTitleBarButtonPosition(nsWindow);
-                        HideZoomButton(nsWindow);
-                    }
-                    catch (Exception exception) {
+                        WindowHandler.RefreshTitleBarButtonPosition(nsWindow);
+                        WindowHandler.HideZoomButton(nsWindow);
+                    } catch (Exception exception) {
                         Console.WriteLine(exception);
                         throw;
                     }
                 }
             }
         }
+#elif Unix
+
+
+#endif
     }
 
-    private async void OnOpened(object sender, EventArgs e) {
+    private void OnOpened(object sender, EventArgs e) {
+#if OSX
         var platform = AssociatedObject.TryGetPlatformHandle();
         if (platform is not null) {
             var nsWindow = platform.Handle;
             if (nsWindow != IntPtr.Zero) {
                 try {
-                    AdjustTitleBarButtonPosition(nsWindow);
-                    HideZoomButton(nsWindow);
+                    WindowHandler.RefreshTitleBarButtonPosition(nsWindow);
+                    WindowHandler.HideZoomButton(nsWindow);
                 }
                 catch (Exception exception) {
                     Console.WriteLine(exception);
@@ -53,66 +63,14 @@ public sealed class WindowTitleBarBehavior : Behavior<Window> {
                 }
             }
         }
-    }
-
-    private void AdjustTitleBarButtonPosition(IntPtr nsWindow) {
-        var selStandardWindowButton = sel_registerName("standardWindowButton:");
-        var selSetFrameOrigin = sel_registerName("setFrameOrigin:");
-
-        if (selStandardWindowButton == IntPtr.Zero || selSetFrameOrigin == IntPtr.Zero) {
-            throw new NullReferenceException();
+#elif LINUX
+        var platform = AssociatedObject.TryGetPlatformHandle();
+        if (platform is not null) {
+            if (IsWayland()) {
+            } else {
+                Platform.Linux.X11.WindowHandler.HideTitleBar(platform.Handle);
+            }
         }
-        
-        var closeButton = objc_msgSend_IntPtr_IntPtr(nsWindow, selStandardWindowButton, (IntPtr)0); // NSWindowButtonClose
-        var minimizeButton = objc_msgSend_IntPtr_IntPtr(nsWindow, selStandardWindowButton, (IntPtr)1); // NSWindowButtonMiniaturize
-        var zoomButton = objc_msgSend_IntPtr_IntPtr(nsWindow, selStandardWindowButton, (IntPtr)2); // NSWindowButtonZoom
-
-        var newCloseButtonPosition = new CGPoint(30, -2);
-        var newMinimizeButtonPosition = new CGPoint(50, -2);
-        var newZoomButtonPosition = new CGPoint(0, 200);
-
-        objc_msgSend_CGPoint(closeButton, selSetFrameOrigin, newCloseButtonPosition);
-        objc_msgSend_CGPoint(minimizeButton, selSetFrameOrigin, newMinimizeButtonPosition);        
-        objc_msgSend_CGPoint(zoomButton, selSetFrameOrigin, newZoomButtonPosition);
-
+#endif
     }
-
-    private void HideZoomButton(IntPtr nsWindow) {
-        var selStandardWindowButton = sel_registerName("standardWindowButton:");
-        var selSetHidden = sel_registerName("setHidden:");
-
-        var zoomButton = objc_msgSend_IntPtr_IntPtr(nsWindow, selStandardWindowButton, (IntPtr)3); // NSWindowButtonZoom
-        
-        objc_msgSend_Bool(zoomButton,selSetHidden, true);
-    }
-    
-    [StructLayout(LayoutKind.Sequential)]
-    public struct CGPoint {
-        public double X;
-        public double Y;
-
-        public CGPoint(double x, double y) {
-            X = x;
-            Y = y;
-        }
-    }
-
-    [DllImport("libobjc.dylib", EntryPoint = "objc_msgSend")] 
-    private static extern IntPtr objc_msgSend_IntPtr_IntPtr(IntPtr receiver, IntPtr selector, IntPtr arg1);
-    
-    [DllImport("libobjc.dylib", EntryPoint = "objc_msgSend")]
-    private static extern void objc_msgSend_CGPoint(IntPtr receiver, IntPtr selector, CGPoint arg1);
-
-    [DllImport("libobjc.dylib", EntryPoint = "objc_msgSend")]
-    private static extern IntPtr objc_msgSend_Bool(IntPtr receiver, IntPtr selector, bool arg1);
-    
-    [DllImport("libobjc.dylib", EntryPoint = "sel_registerName")]
-    private static extern IntPtr sel_registerName(string name);
 }
-
-public static class Dlfcn { 
-    [DllImport("libSystem.B.dylib", EntryPoint = "dlopen")] 
-    public static extern IntPtr dlopen(string path, int mode); 
-
-    [DllImport("libSystem.B.dylib", EntryPoint = "dlsym")] 
-    public static extern IntPtr dlsym(IntPtr handle, string symbol); }
